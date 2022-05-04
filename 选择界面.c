2be84fabf8a14e1166de852e422748cf1de432cd
Doghook <102295939+Doghook@users.noreply.github.com>
2022-05-04 22:45:32 +0800

    Update 选择界面.c

--- 选择界面.c
+++ 选择界面.c
@@ -36,13 +36,7 @@ IMAGE picture2;
 void moshixuanze()
 {
 
-	#include<graphics.h>
-#include<stdio.h>
-#include<mmsystem.h>
-#pragma comment(lib,"winmm.lib")
-#define TextHight 30 //文字高度
-#define TextWidth 30 //文字2宽度
-#define Musicnum 9 //可选歌曲数
+	
 void setgame();
 void setgamemusic();
 void setgamemusic()
