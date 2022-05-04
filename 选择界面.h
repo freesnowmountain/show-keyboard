56b2312d9da79937eb72916de69c060fb362fb13
Doghook <102295939+Doghook@users.noreply.github.com>
2022-05-04 22:46:03 +0800

    Update 选择界面.h

--- 选择界面.h
+++ 选择界面.h
@@ -1,4 +1,10 @@
 #pragma once
 void xuanze();
 void moshixuanze();
-
+#include<graphics.h>
+#include<stdio.h>
+#include<mmsystem.h>
+#pragma comment(lib,"winmm.lib")
+#define TextHight 30 //文字高度
+#define TextWidth 30 //文字2宽度
+#define Musicnum 9 //可选歌曲数
